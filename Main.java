8164f2a8bfc6637897119a61accdd1b4411f0342
rakhars <yoann.r0dyox@gmail.com>
2022-11-18 11:17:02 +0300

    class2

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 import javax.management.relation.Relation;
 
+import manager.FileManager;
+import manager.RequestManager;
+import relationobj.Rel;
+
 public class Main{
     public static void main(String[] args) throws Exception {
         
